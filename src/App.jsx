0493799f192c7e9a13071b26c0dc43657fb6ee16
Glenn <accounts@capitaldiscountfurniture.com>
2026-05-26 01:53:13 -0400

    feat(thank-you): post-purchase landing with polling + license display

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import ShopOS from './pages/ShopOS.jsx'
 import ShopOSSI from './pages/ShopOSSI.jsx'
+import PurchaseThankYou from './pages/PurchaseThankYou.jsx'
+import Consultation from './pages/Consultation.jsx'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/shop-os" element={<ShopOS />} />
       <Route path="/shop-ossi" element={<ShopOSSI />} />
+      <Route path="/shop-ossi/thank-you" element={<PurchaseThankYou />} />
+      <Route path="/consultation" element={<Consultation />} />
       <Route path="*" element={<Home />} />
     </Routes>
   )
